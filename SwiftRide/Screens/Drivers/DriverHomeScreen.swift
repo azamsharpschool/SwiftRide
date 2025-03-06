95f4274baa321e0ed1a5651ed70f86d5b347a6ab
azamsharpschool <155851549+azamsharpschool@users.noreply.github.com>
2025-03-05 21:59:51 -0600

    adding location for driver

--- SwiftRide/Screens/Drivers/DriverHomeScreen.swift
+++ SwiftRide/Screens/Drivers/DriverHomeScreen.swift
@@ -10,13 +10,19 @@ import SwiftUI
 struct DriverHomeScreen: View {
     
     @Environment(SwiftRideStore.self) private var swiftRideStore
+    @Environment(LocationManager.self) private var locationManager
+    
     @State private var isOnline: Bool = false
     
     private func updateDriverStatus() async {
         
+        guard let userLocation = locationManager.userLocation else {
+            return
+        }
+        
         do {
             let user = try await swiftRideStore.currentUser
-            try await swiftRideStore.updateDriverStatus(userId: user.id, isOnline: isOnline, latitude: 10.0, longitude: 20.0)
+            try await swiftRideStore.updateDriverStatus(userId: user.id, isOnline: isOnline, latitude: userLocation.coordinate.latitude, longitude: userLocation.coordinate.longitude)
             
         } catch {
             print(error.localizedDescription)
@@ -28,7 +34,11 @@ struct DriverHomeScreen: View {
             Toggle("Online", isOn: $isOnline)
                 .fixedSize()
             Spacer()
-        }.onChange(of: isOnline) {
+        }
+        .task {
+            locationManager.requestLocation()
+        }
+        .onChange(of: isOnline) {
             Task {
                 // get user location
                 await updateDriverStatus()
@@ -40,5 +50,7 @@ struct DriverHomeScreen: View {
 #Preview {
     NavigationStack {
         DriverHomeScreen()
-    }.environment(SwiftRideStore(client: .development))
+    }
+    .environment(LocationManager())
+    .environment(SwiftRideStore(client: .development))
 }
